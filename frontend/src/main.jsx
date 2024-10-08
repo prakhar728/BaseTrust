b2fb35a0fb4f4c3d26ed1213f02ab5365511d30f
Prakhar <prakharojha12@gmail.com>
2024-10-08 13:39:13 +0530

    Added React Router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import BaseTrustLanding from './pages/LandingPage.jsx'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <BaseTrustLanding />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
